7fb83d8d198871e20fd30e1083fcaf3f7daf96c0
Jordan Walker <jwalker2276@gmail.com>
2018-02-25 11:19:29 -0600

    Added build system and new project.

---
+++ dest/index.js
@@ -0,0 +1,75 @@
+'use strict';
+
+var $animation_elements = $('.animation-element');
+var $win = $(window);
+
+//Onclick event listener for mobile menu button
+$('.nav-btn').click(function () {
+    $('.nav-list').toggleClass("show");
+});
+
+//Onclick event listener for scrolling
+$('.section-link').click(function () {
+    var section = $(this).attr("dest");
+    $('html, body').animate({
+        scrollTop: $('#' + section).offset().top }, 400);
+});
+
+// Onclick event listener for main contact button
+$('#center-info button').click(function () {
+    $('html, body').animate({
+        scrollTop: $('#contact').offset().top }, 800);
+});
+
+//Function checks to see if section is in view for animations
+function check_if_in_view() {
+    var win_height = $win.height();
+    var win_top_position = $win.scrollTop();
+    var win_bottom_position = win_top_position + win_height;
+
+    var offset = 200;
+    var highlight_position = win_top_position + offset;
+
+    $.each($animation_elements, function () {
+        var $element = $(this);
+        var element_height = $element.outerHeight();
+        var element_top_position = $element.offset().top;
+        var element_bottom_position = element_top_position + element_height;
+
+        if (element_bottom_position >= win_top_position && element_top_position <= win_bottom_position) {
+            $element.addClass('animated');
+        }
+    });
+
+    //Check view to see which nav link to highlight
+    if (highlight_position > $('#contact').offset().top) {
+        highlight_nav_link('contact');
+    } else if (highlight_position > $('#about').offset().top) {
+        highlight_nav_link('about');
+    } else if (highlight_position > $('#projects').offset().top) {
+        highlight_nav_link('projects');
+    } else if (highlight_position > $('#skills').offset().top) {
+        highlight_nav_link('skills');
+        border_accent('show');
+    } else if (highlight_position > $('#hero').offset().top) {
+        highlight_nav_link('hero');
+        border_accent('hide');
+    }
+}
+
+//Highlight passed nav link
+function highlight_nav_link(section_name) {
+    $('li.active').removeClass('active');
+    $('li[dest=' + section_name + ']').addClass('active');
+}
+
+function border_accent(command) {
+    if (command === 'show') {
+        $('header nav').addClass('border-accent');
+    } else {
+        $('header nav').removeClass('border-accent');
+    }
+}
+
+$win.on('scroll resize', check_if_in_view);
+$win.trigger('scroll');
--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,71 @@
-var gulp = require('gulp');
-var postcss = require('gulp-postcss');
-var autoprefixer = require('autoprefixer');
+const gulp = require('gulp');
 
-gulp.task('autoprefixer', function () {
-    var processors = [
-        autoprefixer({browsers:['last 2 version']})
-    ];
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
 
-    return gulp.src('./src/css/*.css')
-        .pipe(postcss(processors))
-        .pipe(gulp.dest('./dest'));
-});
+const browserSync = require('browser-sync');
+const server = browserSync.create();
+
+const babel = require('gulp-babel');
+
+const paths = {
+  styles: {
+    src: 'src/css/**/*.css',
+    dest: 'dest/',
+  },
+  scripts: {
+    src: 'src/js/**/*.js',
+    dest: 'dest/',
+  },
+  markup: {
+    src: 'index.html',
+  },
+};
+
+//CSS Pipe
+function styles() {
+  return gulp.src(paths.styles.src)
+    .pipe(postcss([autoprefixer({ browsers: ['last 2 version'] })]))
+    .pipe(gulp.dest(paths.styles.dest));
+}
+
+//JS Pipe
+function scripts() {
+  return gulp.src(paths.scripts.src)
+    .pipe(babel({ presets: ['env'] }))
+    .pipe(gulp.dest(paths.scripts.dest));
+}
+
+//Setup browser
+function serverSetup(done) {
+  server.init({
+    server: {
+      baseDir: './',
+    },
+  });
+  done();
+}
+
+//Browser Sync
+function reload(done) {
+  server.reload();
+  done();
+}
+
+//Watch task
+function watch() {
+  //Watch JS
+  gulp.watch(paths.scripts.src, gulp.series(scripts, reload));
+
+  //Watch CSS
+  gulp.watch(paths.styles.src, gulp.series(styles, reload));
+
+  //Watch HTML
+  gulp.watch(paths.markup.src, reload);
+}
+
+//Default task
+let build = gulp.series(gulp.parallel(styles, scripts), serverSetup, watch);
+
+//Default task
+gulp.task('default', build);
